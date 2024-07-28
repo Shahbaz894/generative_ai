60032d62be303ec31a2bc7b8ef9e5ec0432723e1
shahbaz <shahbazzulfiqar894@gmail.com>
2024-07-28 15:12:40 +0500

    make function

---
+++ main.js
@@ -0,0 +1,19 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log('hey shahbaz hopw are you');
+function addition(a, b) {
+    return a + b;
+}
+function signUp(name, email, password, isPaid) {
+}
+signUp('shahba', 'shahbaz@gmail.com', '12345678', false);
+var aa = addition(44, 22);
+console.log(aa);
+function getValue(myVal) {
+    if (myVal > 5) {
+        return "value greator than 5";
+    }
+    return '200 OK';
+}
+var v = getValue(100);
+console.log(v);
---
+++ main.ts
@@ -0,0 +1,30 @@
+console.log('hey shahbaz hopw are you')
+ function addition(a:number,b:number):number{
+
+    return a+b
+ }
+ function signUp(name:string,email:string,password:string,isPaid:boolean){
+
+ }
+
+signUp(
+    'shahba',
+    'shahbaz@gmail.com',
+    '12345678',
+    false
+)
+ let aa=addition(44,22)
+ console.log(aa)
+
+
+ function getValue(myVal:number){
+    if (myVal>5){
+        return "value greator than 5"
+    }
+    return '200 OK'
+ }
+
+let v=getValue(100)
+console.log(v)
+ export{}
+
